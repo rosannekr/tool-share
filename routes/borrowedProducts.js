7f6314f78a1d6050770a35df5f44b7733895b9ee
samanthapinos <spinosv@gmail.com>
2020-11-05 19:13:27 +0100

    more backend

--- routes/borrowedProducts.js
+++ routes/borrowedProducts.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var models = require("../models")
 
-/* GET borrowed Products */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* ADD product to an user */
+
+router.post("/:id", function (req, res) {
+  const { id } = req.params;
+  const { name, pricePerDay,isAvailable,description, picture, ownerId, CategoryId } = req.body;
+
+  models.Movie.findOne({
+    where: {
+      id,
+    },
+  })
+    .then((product) => {
+      product
+        .createProduct({ name, pricePerDay,isAvailable,description, picture, ownerId, CategoryId })
+        .then((data) => {
+          res.send(data);
+        })
+        .catch((error) => {
+          res.status(500).send(error);
+        });
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
 });
+
 
 module.exports = router;
