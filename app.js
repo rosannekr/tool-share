a8502c49fd84e7a77e8194b89564f7e20a3266d0
Rosanne Kruithof <rosannekruithof@gmail.com>
2020-11-06 13:44:21 +0100

    added user routes

--- app.js
+++ app.js
@@ -1,42 +1,39 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var usersRouter = require('./routes/users');
-var productsRouter = require('./routes/products');
-var categoriesRouter = require('./routes/categories');
-var borrowedProductsRouter = require('./routes/borrowedProducts');
+var usersRouter = require("./routes/users");
+var productsRouter = require("./routes/products");
+var categoriesRouter = require("./routes/categories");
 
 var app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/users', usersRouter);
-app.use('/products', productsRouter);
-app.use('/categories', categoriesRouter);
-app.use('/borrowedProducts', borrowedProductsRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+app.use("/categories", categoriesRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.send('error');
+  res.send("error");
 });
 
 module.exports = app;
--- models/product.js
+++ models/product.js
@@ -14,8 +14,10 @@ module.exports = (sequelize, DataTypes) => {
   Product.associate = function (models) {
     Product.belongsTo(models.Category);
     Product.belongsTo(models.User);
-    Product.belongsToMany(models.User, { through: 'BorrowedProducts', as: 'Borrowers' });
-    
+    Product.belongsToMany(models.User, {
+      through: "BorrowedProducts",
+      as: "Borrowers",
+    });
   };
   return Product;
 };
