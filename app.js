ce2af6aeafc4515d9336596e6f2845ad3af19345
Rosanne Kruithof <rosannekruithof@gmail.com>
2020-11-07 12:37:30 +0100

    register and login routes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var passport = require("passport");
+var session = require("express-session");
 
 var usersRouter = require("./routes/users");
 var productsRouter = require("./routes/products");
@@ -16,6 +18,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// setup for passport
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// setup for routes
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
 app.use("/categories", categoriesRouter);
