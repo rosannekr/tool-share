b90f4f68e5548e6ed9ef6449c41eeef0fecfc5e9
Rosanne Kruithof <rosannekruithof@gmail.com>
2020-11-09 13:43:15 +0100

    added login and signup to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,23 +1,34 @@
 import React from "react";
-import Filters from "./Filters";
 import SearchBar from "./SearchBar";
-import { useHistory } from "react-router-dom";
 import CategoryNav from "./CategoryNav";
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
-  let history = useHistory();
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light py-3">
+        <Link className="navbar-brand ml-3" to="/">
+          Home
+        </Link>
 
-  if (
-    history.location.pathname === "/login" ||
-    history.location.pathname === "/register"
-  ) {
-    return null;
-  } else {
-    return (
-      <div className="container">
-        <SearchBar callback={(products) => props.callback(products)} />
-        <Filters callback={(products) => props.callback(products)} />
-      </div>
-    );
-  }
+        <div className="collapse navbar-collapse">
+          <SearchBar callback={(products) => props.callback(products)} />
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Log In
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <CategoryNav callback={(products) => props.callback(products)} />
+    </div>
+  );
 }
--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
 
 function SearchBar(props) {
-  let history = useHistory();
   let [searchWord, setSearchWord] = useState("");
 
   const handleClick = (e) => {
@@ -11,27 +9,20 @@ function SearchBar(props) {
   };
 
   return (
-    <div className=" mt-4 header ml-4">
-      <div className=" d-flex justify-content-center">
-        <input
-          style={{ width: "750px" }}
-          onChange={handleClick}
-          value={searchWord}
-          type="text"
-          className="form-control search-form mb-5 text-center ml-5"
-          placeholder="Type to search..."
-        />
+    <form className="form-inline mx-auto">
+      <input
+        style={{ width: "750px" }}
+        onChange={handleClick}
+        value={searchWord}
+        type="text"
+        className="form-control text-center"
+        placeholder="Type to search..."
+      />
 
-        <Link to={`/search/${searchWord}`}>
-          <button className="btn underline mb-5 bg-dark">
-            <i className="fa fa-search text-light" aria-hidden="true"></i>
-          </button>
-        </Link>
-      </div>
-      <p>
-        Hi,<Link>username!</Link>
-      </p>
-    </div>
+      <Link to={`/search/${searchWord}`} className="btn underline bg-dark">
+        <i className="fa fa-search text-light" aria-hidden="true"></i>
+      </Link>
+    </form>
   );
 }
 
