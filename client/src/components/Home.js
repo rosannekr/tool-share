21fa888437f064c8ac6bfbefc806648d2e96da2c
Rosanne Kruithof <rosannekruithof@gmail.com>
2020-11-14 20:54:28 +0100

    borrowed route fixed

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
-import Geolocation from "./Geolocation";
 import ProductGrid from "./ProductGrid";
 
 export default function Home(props) {
-  // return <ProductGrid products={props.products} />;
-  return <Geolocation />;
+  return <ProductGrid products={props.products} />;
 }
