a5525fdf01c0794824dbe84c8e787968e6182d0d
samanthapinos <spinosv@gmail.com>
2020-11-15 16:49:45 +0100

    tailwig setup

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -11,7 +11,7 @@ export default function Home(props) {
     <img className="img-fluid w-100 landing-picture" src="/../../../pictures/cover.jpg" />
     <div className="centered rounded"><h1 className="text-info pr-3 pl-3">The art of borrowing</h1>
      <h5 className="pr-3 pl-3 text-muted">save money, borrow your neighbours' stuff!</h5>
-     <Link to="how-it-works"><button className="btn btn-dark text-white">learn how it works</button></Link>
+     <Link to="how-it-works"><button className="bg-purple-400 text-white font-semibold px-4 py-2 rounded">learn how it works</button></Link>
      </div>
   
     </div>
